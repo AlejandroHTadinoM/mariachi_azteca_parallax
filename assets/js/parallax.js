485db8a3aed2cb3fc7967ef5d4a1531c5858ff8e
alejandrohtadinom <alejandrohtadinom@gmail.com>
2017-03-14 14:34:32 -0400

    Navbar position

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,12 +1,30 @@
 var windowHeight = $(window).height();
-var hContainerHeight = $('.banner').height();
+var bannerHeight = $('.banner').height();
 var brandHeight = $('.brand').height();
+var navPos = $('.nav-bar').offset().top;
 
 $(window).scroll(function () {
     var wScroll = $(this).scrollTop();
     console.log(wScroll);
 
-    $('.cta-container').css({
-        'transform': 'translate(0px, ' + (wScroll / 4) + '%)'
-    });
+    if (wScroll <= (bannerHeight - $('.nav-bar').height())) {
+
+        $('.cta-container').css({
+            'transform': 'translate(0px, ' + (wScroll / 4) + '%)'
+        });
+
+    };
+
+    if (wScroll >= navPos - 200) {
+        $('.nav-bar').css({
+            'position': 'fixed',
+            'margin': 'auto'
+        })
+    } else {
+        $('.nav-bar').css({
+            'position': 'relative'
+        })
+    };
+
+    
 });
