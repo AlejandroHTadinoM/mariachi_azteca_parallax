50f8a36983e47ae828315981d5d6a41c45d2e041
alejandrohtadinom <alejandrohtadinom@gmail.com>
2017-04-17 11:28:40 -0400

    Footer links

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -3,10 +3,10 @@ $(window).scroll(function () {
     $('.nav-wrapper').height($('nav').height());
     if (wScroll <= bannerHeight) {
         $('.cta-container').css({
-            'transform': 'translate(0px, ' + (wScroll / 6) + '%)'
+            'transform': 'translate(0px, ' + (wScroll / 3) + '%)'
         });
         $('.banner').css({
-            'background-position': 'center ' + (wScroll / 12) + '%' 
+            'background-position': 'center ' + (wScroll / 6) + '%' 
         });
     };
     if (wScroll > navPos) {
